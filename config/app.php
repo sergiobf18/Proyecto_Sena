088b20e688a3aff9ad8a0296ebc7bc6ede3c1ab2
sergiobf18 <sergiobf@gmail.com>
2024-09-23 12:00:17 -0500

    permisos spatie
    config de spatie para permisos

--- config/app.php
+++ config/app.php
@@ -125,4 +125,4 @@ return [
 
 ];
 
-
+//spatie permission
---
+++ database/seeders/permissionsSeeder.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class permissionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $permisos = [
+            //table admissionHistory
+            'Ver-AdmissionHistory',
+            'Crear-AdmissionHistory',
+            'Editar-AdmissionHistory',
+            'Eliminar-AdmissionHistory',
+            //table appointment
+            'Ver-Appointment',
+            'Crear-Appointment',
+            'Editar-Appointment',
+            'Eliminar-Appointment',
+            //table diagnosis
+            'Ver-Diagnosis',
+            'Crear-Diagnosis',
+            'Editar-Diagnosis',
+            'Eliminar-Diagnosis',
+            //table document
+            'Ver-Document',
+            'Crear-Document',
+            'Editar-Document',
+            'Eliminar-Document',
+            //table followUpHistory
+            'Ver-FollowUpHistory',
+            'Crear-FollowUpHistory',
+            'Editar-FollowUpHistory',
+            'Eliminar-FollowUpHistory',
+            //table patient
+            'Ver-Patient',
+            'Crear-Patient',
+            'Editar-Patient',
+            'Eliminar-Patient',
+            //table psychologist
+            'Ver-Psychologist',
+            'Crear-Psychologist',
+            'Editar-Psychologist',
+            'Eliminar-Psychologist',
+             //table psychologist
+             'Ver-Psychologist',
+             'Crear-Psychologist',
+             'Editar-Psychologist',
+             'Eliminar-Psychologist',
+        ];
+    }
+}
